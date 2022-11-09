7e8a63493cbf919e60846632bac497fb3e33ded2
Rhelimi Axel <axel.rhelimi@gmail.com>
2022-11-09 20:16:35 +0100

    restructure

---
+++ scripts/gameMod.js
@@ -0,0 +1,23 @@
+export class GameMod1
+{
+    getThreeRandomCard()
+    {
+
+    }
+}
+
+export class GameMod2
+{
+    getThreeRandomCard()
+    {
+
+    }
+}
+
+export class GameMod3
+{
+    getThreeRandomCard()
+    {
+
+    }
+}
